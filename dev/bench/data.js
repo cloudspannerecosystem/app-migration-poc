ad2695f50898f15a7c7123e39d5e49034ab929d7
github-action-benchmark <github@users.noreply.github.com>
2024-10-09 15:38:29 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for bd76790cde373c93eb4ded9d7ed5f0b82029f356

---
+++ dev/bench/data.js
@@ -0,0 +1,33 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1728488309594,
+  "repoUrl": "https://github.com/cloudspannerecosystem/app-migration-poc",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "cloudspannerecosystem",
+            "username": "cloudspannerecosystem"
+          },
+          "committer": {
+            "name": "cloudspannerecosystem",
+            "username": "cloudspannerecosystem"
+          },
+          "id": "bd76790cde373c93eb4ded9d7ed5f0b82029f356",
+          "message": "Add a workflow to run evals automatically",
+          "timestamp": "2024-10-07T09:15:35Z",
+          "url": "https://github.com/cloudspannerecosystem/app-migration-poc/pull/5/commits/bd76790cde373c93eb4ded9d7ed5f0b82029f356"
+        },
+        "date": 1728488308996,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Overall Eval Result",
+            "value": 33.33333333333333,
+            "unit": "Accuracy (percent)"
+          }
+        ]
+      }
+    ]
+  }
+}
